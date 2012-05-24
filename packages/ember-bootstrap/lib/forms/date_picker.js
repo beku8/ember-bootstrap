2f6fc2fab8220d3940b2f0fe1604f73592d10a7a
Pieter-Jan Lammertyn <pjlammertyn@gmail.com>
2012-05-24 10:53:12 +0200

    made datepicker work with bootstrap.datepicker
    Signed-off-by: Pieter-Jan Lammertyn <pjlammertyn@gmail.com>

--- packages/ember-bootstrap/lib/forms/date_picker.js
+++ packages/ember-bootstrap/lib/forms/date_picker.js
@@ -6,20 +6,68 @@ require("ember-bootstrap/forms/field");
 	data-date-language: 'nl'
 });*/
 
+
+
 Bootstrap.Forms.DatePicker = Bootstrap.Forms.Field.extend({
   dataDateFormat: 'dd-mm-yyyy',
   dataDateLanguage: 'nl',
 
   inputField: Ember.TextField.extend({
-    valueBinding: 'parentView.value',
+    //valueBinding: 'parentView.value',
     nameBinding: 'parentView.label',
     dataDateFormatBinding: 'parentView.dataDateFormat',
 	dataDateLanguageBinding: 'parentView.dataDateLanguage',
-    attributeBindings: ['name', 'dataDateFormat', 'dataDateLanguage'],
+    attributeBindings: ['name', 'dataDateFormat:data-date-format', 'dataDateLanguage:data-date-language', 'type', 'value'],
     
-	didInsertElement: function() {
-      this.$().datepicker();
-    }
+	//didInsertElement: function() {
+	//	Ember.run.next(this, function() {
+	//		this.$().datepicker();
+	//		var datepicker = this.$().data('datepicker');
+	//		if (!Ember.empty(datepicker)) {
+	//		    datepicker.date = this.get('value');
+    //		   	datepicker.setValue();
+    //		   	datepicker.update();
+    //	   	}
+	//	});
+    //},
+    
+    //valueChanged: function() {
+    //	var datepicker = this.$().data('datepicker');
+    //	if (!Ember.empty(datepicker)) {
+	//		datepicker.date = this.get('value');
+    //		datepicker.setValue()grep;
+    //		datepicker.update();
+    //	}
+  	//}.observes('value')  
+  	
+  	valueBinding: Ember.Binding.transform({
+        to: function(value, binding) {  
+        	if (Ember.typeOf(value) === 'date') {
+        		var format = this.$().datepicker.DPGlobal.parseFormat(binding.get('dataDateFormat'));
+        		return this.$().datepicker.DPGlobal.formatDate(value, 
+					format, 
+					binding.get('dataDateLanguage'))
+        	}
+            return value;
+        },
+        from: function(value, binding) {
+			if (Ember.typeOf(value) === 'date') {
+        		var format = this.$().datepicker.DPGlobal.parseFormat(binding.get('dataDateFormat'));
+        		return this.$().datepicker.DPGlobal.parseDate(value, 
+        			format, 
+					binding.get('dataDateLanguage'));
+			}
+            return value;
+        }
+    }).from('date'),
+  	
+  	dateBinding: 'parentView.value',
+  	
+  	didInsertElement: function() {
+		Ember.run.next(this, function() {
+			this.$().datepicker();
+		});
+    } 	
   })
 });
 
