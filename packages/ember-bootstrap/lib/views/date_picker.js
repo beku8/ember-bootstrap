fa46f0239fcec484054e2617b9013ecd521aac1a
Pieter-Jan Lammertyn <pjlammertyn@gmail.com>
2012-06-08 08:37:48 +0200

    modified datepicker to use computed properties instead of binding transformer
    Signed-off-by: Pieter-Jan Lammertyn <pjlammertyn@gmail.com>

--- packages/ember-bootstrap/lib/views/date_picker.js
+++ packages/ember-bootstrap/lib/views/date_picker.js
@@ -6,26 +6,28 @@ Bootstrap.DatePicker = Ember.TextField.extend({
   	dataDateLanguage: 'nl',
     attributeBindings: ['name', 'dataDateFormat:data-date-format', 'dataDateLanguage:data-date-language', 'type', 'value'],
     
-  	valueBinding: Ember.Binding.transform({
-        to: function(value, binding) {  
-        	if (Ember.typeOf(value) === 'date') {
-        		var format = this.$().datepicker.DPGlobal.parseFormat(binding.get('dataDateFormat'));
-        		return this.$().datepicker.DPGlobal.formatDate(value, 
-					format, 
-					binding.get('dataDateLanguage'))
-        	}
-            return value;
-        },
-        from: function(value, binding) {
-			if (Ember.typeOf(value) === 'date') {
-        		var format = this.$().datepicker.DPGlobal.parseFormat(binding.get('dataDateFormat'));
-        		return this.$().datepicker.DPGlobal.parseDate(value, 
+	value: Ember.computed(function(key, value) {
+		// getter
+		if (arguments.length === 1) {
+		  	var date = this.get('date');
+			if (Ember.typeOf(date) === 'date') {
+        		var format = this.$().datepicker.DPGlobal.parseFormat(this.get('dataDateFormat'));
+        		return this.$().datepicker.DPGlobal.formatDate(date, 
         			format, 
-					binding.get('dataDateLanguage'));
+					this.get('dataDateLanguage'));
 			}
-            return value;
-        }
-    }).from('date'),
+            return date;
+		// setter
+		} else {
+        	if (Ember.typeOf(value) === 'string') {
+        		var format = this.$().datepicker.DPGlobal.parseFormat(this.get('dataDateFormat'));
+        		this.set('date', this.$().datepicker.DPGlobal.parseDate(value, 
+					format, 
+					thid.get('dataDateLanguage')));
+        	}
+            return value;		  	
+		}
+	}).property('date'),
   	  	
   	didInsertElement: function() {
         this._super();
