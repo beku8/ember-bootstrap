898fbd49f7bc0931559031cf829b42e7bff58656
Pieter-Jan Lammertyn <pjlammertyn@gmail.com>
2012-06-12 16:16:18 +0200

    changes to the last ember
    Signed-off-by: Pieter-Jan Lammertyn <pjlammertyn@gmail.com>

--- packages/ember-bootstrap/lib/views/tabs.js
+++ packages/ember-bootstrap/lib/views/tabs.js
@@ -5,3 +5,16 @@ Bootstrap.Tabs = Ember.CollectionView.extend({
   itemViewClass: Bootstrap.PillItem,
   selection: null
 });
+
+
+/*Bootstrap.TabContainerView = Ember.TabContainerView.extend({
+});*/
+
+Bootstrap.TabView = Ember.TabView.extend({
+	tagName: 'li',
+	classNameBindings: ['isActive:active'],
+	
+	isActive: Ember.computed(function() {
+    	return this.get('value') === this.getPath('tabsContainer.currentView');
+  	}).property('tabsContainer.currentView')
+});
