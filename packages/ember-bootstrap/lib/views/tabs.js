f288fdaea872ba98f7f36e9e0d7fc740698c27fe
Pieter-Jan Lammertyn <pjlammertyn@gmail.com>
2013-01-17 10:00:10 +0100

    remove ember-tab

--- packages/ember-bootstrap/lib/views/tabs.js
+++ packages/ember-bootstrap/lib/views/tabs.js
@@ -1,23 +1,43 @@
 var Bootstrap = window.Bootstrap;
+
+Bootstrap.TabContainerView = Ember.View.extend({
+});
+
+Bootstrap.TabView = Ember.View.extend({
+  tagName: 'li',
+  classNameBindings: ['isActive:active'],
+
+  tabsContainer: Ember.computed(function () {
+    return this.nearestInstanceOf(Bootstrap.TabContainerView);
+  }).property().volatile(),
+
+  mouseUp: function () {
+    setPath(this, 'tabsContainer.currentView', get(this, 'value'));
+  },
+
+  isActive: function() {
+    return this.get('value') === this.get('tabsContainer.currentView');
+  }.property('tabsContainer.currentView').cacheable()
+});
+
+Bootstrap.TabPaneView = Ember.View.extend({
+  tabsContainer: Ember.computed(function () {
+    return this.nearestOfType(Ember.TabContainerView);
+  }).property().volatile(),
+
+  isVisible: Ember.computed(function () {
+    return get(this, 'viewName') === get(this, 'tabsContainer.currentView');
+  }).property('tabsContainer.currentView').volatile()
+});
+
+
+
 Bootstrap.Tabs = Ember.CollectionView.extend({
   classNames: ['nav', 'nav-tabs'],
   classNameBindings: ['isStacked:nav-stacked'],
   tagName: 'ul',
   itemViewClass: Bootstrap.PillItem,
   selection: null
-});
-
-
-/*Bootstrap.TabContainerView = Ember.TabContainerView.extend({
-});*/
-
-Bootstrap.TabView = Ember.TabView.extend({
-	tagName: 'li',
-	classNameBindings: ['isActive:active'],
-	
-	isActive: function() {
-    	return this.get('value') === this.get('tabsContainer.currentView');
-  	}.property('tabsContainer.currentView').cacheable()
 });
 
 Bootstrap.TabItem = Ember.View.extend({
